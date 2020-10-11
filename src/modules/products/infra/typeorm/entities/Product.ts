00b200179989277c4fe6b3899f64c94660066463
Pedro Tassinari <pedro.tassinari99@gmail.com>
2020-10-11 18:25:14 -0300

    organizando entities

--- src/modules/products/infra/typeorm/entities/Product.ts
+++ src/modules/products/infra/typeorm/entities/Product.ts
@@ -4,24 +4,34 @@ import {
   PrimaryGeneratedColumn,
   CreateDateColumn,
   UpdateDateColumn,
+  JoinColumn,
   OneToMany,
 } from 'typeorm';
 
 import OrdersProducts from '@modules/orders/infra/typeorm/entities/OrdersProducts';
 
+@Entity('products')
 class Product {
+  @PrimaryGeneratedColumn('uuid')
   id: string;
 
+  @Column('string')
   name: string;
 
+  @Column('decimal')
   price: number;
 
+  @Column('integer')
   quantity: number;
 
+  @OneToMany(() => OrdersProducts, ordersProducts => ordersProducts.product)
+  @JoinColumn({ name: 'product_id' })
   order_products: OrdersProducts[];
 
+  @CreateDateColumn()
   created_at: Date;
 
+  @UpdateDateColumn()
   updated_at: Date;
 }
 
