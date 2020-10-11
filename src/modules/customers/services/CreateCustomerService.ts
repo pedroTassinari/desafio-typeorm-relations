b608aeff5d1922db2dfd0fa4accdf74445368ff7
Pedro Tassinari <pedro.tassinari99@gmail.com>
2020-10-11 15:12:29 -0300

    controller e service para createCustomer

--- src/modules/customers/services/CreateCustomerService.ts
+++ src/modules/customers/services/CreateCustomerService.ts
@@ -12,10 +12,24 @@ interface IRequest {
 
 @injectable()
 class CreateCustomerService {
-  constructor(private customersRepository: ICustomersRepository) {}
+  constructor(
+    @inject('CustomersRepository')
+    private customersRepository: ICustomersRepository,
+  ) {}
 
   public async execute({ name, email }: IRequest): Promise<Customer> {
-    // TODO
+    const isEmailUsed = await this.customersRepository.findByEmail(email);
+
+    if (isEmailUsed) {
+      throw new AppError('This email have been used by another user');
+    }
+
+    const customer = await this.customersRepository.create({
+      name,
+      email,
+    });
+
+    return customer;
   }
 }
 
